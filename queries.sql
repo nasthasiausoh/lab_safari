c6acea6de7116645f85f70ede6be496badf1ab81
Kelly Wong <kellykl_wong@hotmail.com>
2023-04-14 15:20:05 +0100

    adds first two extension tasks

--- queries.sql
+++ queries.sql
@@ -10,3 +10,16 @@
 -- INNER JOIN enclosures
 -- ON assignments.enclosure_id = enclosures.id;
 
+-- NAMES OF STAFF WORKING IN ENCLOSURES WHICH ARE CLOSED FOR MAINTENACE
+-- SELECT employees.name, assignments.enclosure_id, enclosures.name FROM enclosures
+-- INNER JOIN assignments
+-- ON assignments.enclosure_id = enclosures.id
+-- INNER JOIN employees
+-- ON employees.id = assignments.employee_id
+-- WHERE enclosures.closed_for_maintenance = TRUE;
+
+-- NAME OF ENCLOSURE WHERE THE OLDEST ANIMAL LIVES
+SELECT animals.name, animals.age, animals.enclosure_id, enclosures.name FROM animals
+INNER JOIN enclosures 
+ON animals.enclosure_id = enclosures.id
+ORDER BY animals.age DESC, animals.name ASC ;
