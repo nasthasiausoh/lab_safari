2f901daa394a63025b987cc87a89714b278d4f7e
Kelly Wong <kellykl_wong@hotmail.com>
2023-04-14 15:27:42 +0100

    adds solution to extension task 3

--- queries.sql
+++ queries.sql
@@ -19,7 +19,17 @@
 -- WHERE enclosures.closed_for_maintenance = TRUE;
 
 -- NAME OF ENCLOSURE WHERE THE OLDEST ANIMAL LIVES
-SELECT animals.name, animals.age, animals.enclosure_id, enclosures.name FROM animals
-INNER JOIN enclosures 
-ON animals.enclosure_id = enclosures.id
-ORDER BY animals.age DESC, animals.name ASC ;
+-- SELECT animals.name, animals.age, animals.enclosure_id, enclosures.name FROM animals
+-- INNER JOIN enclosures 
+-- ON animals.enclosure_id = enclosures.id
+-- ORDER BY animals.age DESC, animals.name ASC;
+
+-- NUMBER OF DIFFERENT ANIMAL TYPES A GIVEN KEEPER HAS BEEN ASSIGNED
+SELECT employees.name, COUNT(DISTINCT animals.type) FROM employees
+INNER JOIN assignments
+ON employees.id = assignments.employee_id
+INNER JOIN enclosures
+ON assignments.enclosure_id = enclosures.id
+INNER JOIN animals
+ON enclosures.id = animals.enclosure_id
+GROUP BY employees.name;
